becc112b65c2bbc4505ff686b40c1cb04387b9cf
jadaun0079 <jatinpratapsingh1000@gmail.com>
2026-01-21 19:11:04 +0530

    2nd question

---
+++ nodejs-programming/question2/callfile.js
@@ -0,0 +1,5 @@
+const stringUtils = require('./stringutils');
+
+console.log(stringutils.capitalize('hello')); // Output: Hello
+console.log(stringutils.reverse('hello'));    // Output: olleh
+console.log(stringutils.countVowels('hello')); // Output: 2
